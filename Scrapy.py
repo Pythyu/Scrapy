90b168919c6e7bbacd556211c252ea90446607e2
antoine.zellmeyer <antoine.zellmeyer@epita.fr>
2022-04-06 09:11:00 +0200

    Better Standalone Fonctionality

--- Scrapy.py
+++ Scrapy.py
@@ -15,6 +15,7 @@ class Scrapper:
         """
         self.url = url
         self.folder = folder
+        self.session = requests.Session()
 
     def scrap_links_in_url(self):
         """
@@ -23,10 +24,9 @@ class Scrapper:
 
         :return: List of string corresponding to all links in the webpage
         """
-        session = requests.Session()
-        session.headers[
+        self.session.headers[
             'User-Agent'] = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/34.0.1847.131 Safari/537.36'
-        page_content = session.get(self.url).content
+        page_content = self.session.get(self.url).content
         soup = BeautifulSoup(page_content, "html.parser")
         return [link["href"] for link in soup.find_all("a", href=True)]
 
@@ -74,22 +74,43 @@ class Scrapper:
 
 
 
+def download_url_links_content(scrap):
+    """
+    Download all content from all links found into an url
+    """
+    links = scrap.scrap_links_in_url()
+
+    scrap.retrieve_urls(links)
+    return "Retrieve Successful !"
+
+def get_all_links(scrap):
+    return scrap.scrap_links_in_url()
+
+def demo(scrap):
+    scrap = Scrapper("http://index-of.es/Attacks/403%20Forbidden%20Attack/", "data")
+
+    links = scrap.scrap_links_in_url()
+
+    scrap.retrieve_urls(links)
+    return "Demo Over, you can found newly downloaded pdf into %s" % (scrap.folder)
+
+features = [download_url_links_content, demo, get_all_links]
+
 # Main
 if __name__ == "__main__":
     argc = len(sys.argv)
-    if argc <= 1:
-        scrap = Scrapper("http://index-of.es/Attacks/403%20Forbidden%20Attack/", "data")
-
-        links = scrap.scrap_links_in_url()
-
-        scrap.retrieve_urls(links)
-    elif argc != 3:
-        print("usage : ./python3 Scrapy.py <url> <folder>")
+    if argc != 4:
+        print("usage : ./python3 Scrapy.py <actionID> <url> <folder>")
+        print("   > actionID: Number of the desired action to be executed by Scrapy")
+        print("       > 0 : Download all content from all links found into an url")
+        print("       > 1 : Demonstration of Scrapy by downloading some files from a website")
+        print("       > 2 : Get all links contained within an url")
         print("   > url: String of the targeted website")
-        print("   > folder: Folder where link's content will be stored")
+        print("   > folder: Folder to store possible outputs (downloads, files, ...)")
     else:
-        scrap = Scrapper(sys.argv[1], sys.argv[2])
-
-        links = scrap.scrap_links_in_url()
-
-        scrap.retrieve_urls(links)
+        scrap = Scrapper(sys.argv[2], sys.argv[3])
+        ID = int(sys.argv[1])
+        try:
+            print(features[ID](scrap))
+        except IndexError:
+            print("/!\\ =====> Invalid action ID : %d isn't supported <===== /!\\" % (ID))
